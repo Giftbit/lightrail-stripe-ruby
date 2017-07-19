8cbaa8c547b99eb6cfb5cb673bc8e40334b21d6a
Tana Jukes <tana.j@lightrail.com>
2017-07-19 10:08:52 -0700

    Add Refund class to refund a transaction
    Uses the return value from GiftCharge.create or GiftCharge.capture (requires cardId and transactionId).

--- lib/lightrail_client_ruby.rb
+++ lib/lightrail_client_ruby.rb
@@ -9,6 +9,7 @@ require "lightrail_client_ruby/ping"
 require "lightrail_client_ruby/gift_value"
 require "lightrail_client_ruby/gift_charge"
 require "lightrail_client_ruby/gift_fund"
+require "lightrail_client_ruby/refund"
 
 module LightrailClientRuby
 end
---
+++ spec/refund_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+RSpec.describe LightrailClientRuby::Refund do
+
+  describe ".create" do
+    it "refunds a transaction" do
+      charge_object = {
+          amount: 1,
+          currency: 'USD',
+          userSuppliedId: 'ruby-test-' + rand().to_s,
+          code: ENV['TEST_CODE'],
+          capture: true,
+      }
+      transaction_object = LightrailClientRuby::GiftCharge.create(charge_object)
+      refund_response = LightrailClientRuby::Refund.create(transaction_object)
+      expect(refund_response['transaction']['transactionType']).to eq('DRAWDOWN_REFUND')
+    end
+  end
+
+end

7b6ab299a08b7e2a55008a42b166bdde10834d7f
Tana Jukes <tana.j@lightrail.com>
2017-07-27 11:30:52 -0700

    Improve Connection spec
    Split expectations for header content and added failure messages for each item. Removed spec to check for the API base URL: to follow the convention set by Stripe, this info should be set as a variable on the main gem module, not in an included class.

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -2,20 +2,15 @@ require "spec_helper"
 
 RSpec.describe LightrailClientRuby::Connection do
 
-  describe ".api_base" do
-    it "returns the base URL for the Lightrail API" do
-      expect(LightrailClientRuby::Connection.api_base).to eq('https://dev.lightrail.com/v1')
-    end
-  end
-
   describe ".connection" do
     it "has the right base URL" do
       expect(LightrailClientRuby::Connection.connection.url_prefix.to_s).to eq(LightrailClientRuby::Connection.api_base)
     end
 
     it "has the right headers" do
-      expect(LightrailClientRuby::Connection.connection.headers['Content-Type']).to eq('application/json; charset=utf-8')
-      # expect(LightrailClientRuby::Connection.connection.headers['Authorization'].to eq('Bearer'))
+      expect(LightrailClientRuby::Connection.connection.headers['Content-Type']).to include('application/json'), "expected Content-Type header to include 'application/json'"
+      expect(LightrailClientRuby::Connection.connection.headers['Content-Type']).to include('charset=utf-8'), "expected Content-Type header to include 'charset=utf-8'"
+      expect(LightrailClientRuby::Connection.connection.headers['Authorization']).to include('Bearer'), "expected Authorization header to include 'Bearer'"
     end
   end
 

8dc844ef9b38d3983b4a5f21ce291bdf670f8962
Tana Jukes <tana.j@lightrail.com>
2017-07-27 09:36:38 -0700

    Add param validation to Refund.create

--- lib/lightrail_client_ruby/refund.rb
+++ lib/lightrail_client_ruby/refund.rb
@@ -1,16 +1,21 @@
 module LightrailClientRuby
   class Refund
 
-    def self.create(transaction_object)
-      card_id = transaction_object['transaction']['cardId']
-      transaction_id = transaction_object['transaction']['transactionId']
+    def self.create(original_transaction_response)
+      if LightrailClientRuby::Validator.is_valid_transaction_response? (original_transaction_response)
+        card_id = original_transaction_response['transaction']['cardId']
+        transaction_id = original_transaction_response['transaction']['transactionId']
 
-      url = LightrailClientRuby::Connection.api_endpoint_refund_transaction(card_id, transaction_id)
-      body = {
-          userSuppliedId: "#{transaction_id}-refund"
-      }
+        url = LightrailClientRuby::Connection.api_endpoint_refund_transaction(card_id, transaction_id)
+        body = {
+            userSuppliedId: "#{transaction_id}-refund"
+        }
 
-      LightrailClientRuby::Connection.make_post_request_and_parse_response(url, body)
+        LightrailClientRuby::Connection.make_post_request_and_parse_response(url, body)
+
+      else
+        raise ArgumentError.new("Invalid original_transaction_response")
+      end
     end
 
   end
--- spec/refund_spec.rb
+++ spec/refund_spec.rb
@@ -1,19 +1,30 @@
 require "spec_helper"
 
 RSpec.describe LightrailClientRuby::Refund do
-
   describe ".create" do
-    it "refunds a transaction" do
-      charge_object = {
-          amount: 1,
-          currency: 'USD',
-          code: ENV['TEST_CODE'],
-          capture: true,
-      }
-      transaction_object = LightrailClientRuby::GiftCharge.create(charge_object)
-      refund_response = LightrailClientRuby::Refund.create(transaction_object)
-      expect(refund_response['transaction']['transactionType']).to eq('DRAWDOWN_REFUND')
-    end
-  end
 
+    context "when given valid params" do
+      it "refunds a transaction" do
+        charge_object = {
+            amount: 1,
+            currency: 'USD',
+            code: ENV['TEST_CODE'],
+            capture: true,
+        }
+        transaction_object = LightrailClientRuby::GiftCharge.create(charge_object)
+        refund_response = LightrailClientRuby::Refund.create(transaction_object)
+        expect(refund_response['transaction']['transactionType']).to eq('DRAWDOWN_REFUND')
+      end
+    end
+
+    context "when given bad/missing params" do
+      it "throws an error when required params are missing" do
+        expect {LightrailClientRuby::Refund.create()}.to raise_error(ArgumentError), "called Refund.create with no params"
+        expect {LightrailClientRuby::Refund.create({})}.to raise_error(ArgumentError), "called Refund.create with empty object"
+        expect {LightrailClientRuby::Refund.create({card: ENV['TEST_CARD_ID']})}.to raise_error(ArgumentError), "called Refund.create with '{card: ENV['TEST_CARD_ID']}'"
+        expect {LightrailClientRuby::Refund.create([])}.to raise_error(ArgumentError), "called Refund.create with empty array"
+      end
+    end
+
+  end
 end

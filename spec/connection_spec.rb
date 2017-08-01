4b1026f834f3a3bf80dc1ac0f61cf52980a20210
Tana Jukes <tana.j@lightrail.com>
2017-07-31 17:37:37 -0700

    Improve Connection test to cover new errors
    Also added 'subject' to enhance spec readability, and changed previous 'let' definition

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -1,28 +1,54 @@
 require "spec_helper"
 
 RSpec.describe LightrailClientRuby::Connection do
+  subject(:connection) {LightrailClientRuby::Connection}
 
   describe ".connection" do
-    let(:connection) {LightrailClientRuby::Connection.connection}
+    let(:conn) {LightrailClientRuby::Connection.connection}
 
     it "has the right base URL" do
-      expect(connection.url_prefix.to_s).to eq(LightrailClientRuby.api_base)
+      expect(conn.url_prefix.to_s).to eq(LightrailClientRuby.api_base)
     end
 
     it "has the right headers" do
-      expect(connection.headers['Content-Type']).to include('application/json'), "expected Content-Type header to include 'application/json'"
-      expect(connection.headers['Content-Type']).to include('charset=utf-8'), "expected Content-Type header to include 'charset=utf-8'"
-      expect(connection.headers['Authorization']).to include('Bearer'), "expected Authorization header to include 'Bearer'"
+      expect(conn.headers['Content-Type']).to include('application/json'), "expected Content-Type header to include 'application/json'"
+      expect(conn.headers['Content-Type']).to include('charset=utf-8'), "expected Content-Type header to include 'charset=utf-8'"
+      expect(conn.headers['Authorization']).to include('Bearer'), "expected Authorization header to include 'Bearer'"
     end
   end
 
+
   describe ".make_get_request_and_parse_response" do
+    context "when given good params" do
+      # ...
+    end
+
+
     context "bad API response" do
-      it "should throw an error" do
-        response_401 = Faraday::Response.new(status: 401, body: "{\"status\":401,\"message\":\"Unauthorized\"}")
-        allow(LightrailClientRuby::Connection).to receive_message_chain('connection.get').and_return(response_401)
-        expect {LightrailClientRuby::Connection.make_get_request_and_parse_response('ping')}.to raise_error(LightrailClientRuby::AuthenticationError)
+      it "should throw a LightrailError with response details when status is not 200" do
+        bad_response = Faraday::Response.new(status: 418, body: "{\"status\":418,\"message\":\"I'm a teapot\"}")
+        allow(connection).to receive_message_chain('connection.get').and_return(bad_response)
+        expect {connection.make_get_request_and_parse_response('ping')}.to raise_error(LightrailClientRuby::LightrailError, /teapot/)
       end
+
+      it "should throw an AuthorizationError when status is 401 or 403" do
+        response_401 = Faraday::Response.new(status: 401, body: "{\"status\":401,\"message\":\"Unauthorized\"}")
+        allow(connection).to receive_message_chain('connection.get').and_return(response_401)
+        expect {connection.make_get_request_and_parse_response('ping')}.to raise_error(LightrailClientRuby::AuthorizationError)
+      end
+
+      it "should throw a CouldNotFindObjectError when status is 404" do
+        response_404 = Faraday::Response.new(status: 404, body: "{\"status\":404,\"message\":\"Could not find object\"}")
+        allow(connection).to receive_message_chain('connection.get').and_return(response_404)
+        expect {connection.make_get_request_and_parse_response('ping')}.to raise_error(LightrailClientRuby::CouldNotFindObjectError)
+      end
+
+      it "should throw a BadParameterError when status is 409" do
+        response_409 = Faraday::Response.new(status: 409, body: "{\"status\":409,\"message\":\"Bad parameter\"}")
+        allow(connection).to receive_message_chain('connection.get').and_return(response_409)
+        expect {connection.make_get_request_and_parse_response('ping')}.to raise_error(LightrailClientRuby::BadParameterError)
+      end
+
     end
   end
 

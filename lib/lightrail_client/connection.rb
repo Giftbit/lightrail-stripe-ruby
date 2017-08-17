92d41a8ab8af61afb479aadf5256c67a8554d5fa
Tana Jukes <tana.j@lightrail.com>
2017-08-17 09:40:54 -0700

    Rearrange connection methods
    No more need to publicly expose .connection, .make_post_request_and_parse_response, .make_get_request_and_parse_response, or .handle_response now that they are wrapped by more specific helper methods.

--- lib/lightrail_client/connection.rb
+++ lib/lightrail_client/connection.rb
@@ -1,6 +1,37 @@
 module LightrailClient
   class Connection
 
+    def self.ping
+      self.send :make_get_request_and_parse_response, "ping"
+    end
+
+    def self.get_code_balance(code)
+      self.send :make_get_request_and_parse_response, "codes/#{code}/balance/details"
+    end
+
+    def self.get_card_id_balance(card_id)
+      self.send :make_get_request_and_parse_response, "cards/#{card_id}/balance"
+    end
+
+    def self.make_code_transaction(code, charge_params)
+      self.send :make_post_request_and_parse_response, "codes/#{code}/transactions", charge_params
+    end
+
+    def self.make_card_id_transaction(card_id, charge_params)
+      self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions", charge_params
+    end
+
+    def self.handle_pending(card_id, transaction_id, void_or_capture, request_body)
+      self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions/#{transaction_id}/#{void_or_capture}", request_body
+    end
+
+    def self.post_refund(card_id, transaction_id, request_body)
+      self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions/#{transaction_id}/refund", request_body
+    end
+
+
+    private
+
     def self.connection
       conn = Faraday.new LightrailClient.api_base, ssl: {version: :TLSv1_2}
       conn.headers['Content-Type'] = 'application/json; charset=utf-8'
@@ -44,35 +75,6 @@ module LightrailClient
         else
           raise LightrailError.new("Server responded with: (#{response.status}) #{message}", response)
       end
-    end
-
-
-    def self.ping
-      self.send :make_get_request_and_parse_response, "ping"
-    end
-
-    def self.get_code_balance(code)
-      self.send :make_get_request_and_parse_response, "codes/#{code}/balance/details"
-    end
-
-    def self.get_card_id_balance(card_id)
-      self.send :make_get_request_and_parse_response, "cards/#{card_id}/balance"
-    end
-
-    def self.make_code_transaction(code, charge_params)
-      self.send :make_post_request_and_parse_response, "codes/#{code}/transactions", charge_params
-    end
-
-    def self.make_card_id_transaction(card_id, charge_params)
-      self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions", charge_params
-    end
-
-    def self.handle_pending(card_id, transaction_id, void_or_capture, request_body)
-      self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions/#{transaction_id}/#{void_or_capture}", request_body
-    end
-
-    def self.post_refund(card_id, transaction_id, request_body)
-      self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions/#{transaction_id}/refund", request_body
     end
 
   end

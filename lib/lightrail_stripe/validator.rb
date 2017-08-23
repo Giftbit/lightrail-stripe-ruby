b1bd9e085a31864061781bf33c361178b7774ac4
Tana Jukes <tana.j@lightrail.com>
2017-08-23 12:56:58 -0700

    Condense validate/translate steps into one for drawdown params
    First step towards combining overlapping functionality in Validator & Translator classes.

--- lib/lightrail_stripe/validator.rb
+++ lib/lightrail_stripe/validator.rb
@@ -1,11 +1,35 @@
 module Lightrail
   class Validator
+    def self.set_params_for_code_drawdown!(charge_params)
+      validated_params = charge_params.clone
+      begin
+        return validated_params if ((validated_params.is_a? Hash) &&
+            self.set_code!(validated_params) &&
+            self.validate_amount!(validated_params[:amount] || validated_params[:value]) &&
+            self.validate_currency!(validated_params[:currency])) &&
+            self.get_or_set_userSuppliedId!(validated_params)
+      rescue Lightrail::LightrailArgumentError
+      end
+      raise Lightrail::LightrailArgumentError.new("Invalid charge_params for set_params_for_code_drawdown!: #{charge_params.inspect}")
+    end
+
+    def self.set_params_for_card_id_drawdown!(charge_params)
+      validated_params = charge_params.clone
+      begin
+        return validated_params if ((validated_params.is_a? Hash) &&
+            self.set_cardId!(validated_params) &&
+            self.validate_amount!(validated_params[:amount] || validated_params[:value]) &&
+            self.validate_currency!(validated_params[:currency])) &&
+            self.get_or_set_userSuppliedId!(validated_params)
+      rescue Lightrail::LightrailArgumentError
+      end
+      raise Lightrail::LightrailArgumentError.new("Invalid charge_params for set_params_for_card_id_drawdown!: #{charge_params.inspect}")
+    end
+
     def self.validate_charge_object! (charge_params)
       begin
-        return true if ((charge_params.is_a? Hash) &&
-            (self.has_valid_code?(charge_params) || self.has_valid_card_id?(charge_params)) &&
-            self.validate_amount!(charge_params[:amount] || charge_params[:value]) &&
-            self.validate_currency!(charge_params[:currency]))
+        return true if (self.set_params_for_code_drawdown!(charge_params) if self.has_valid_code?(charge_params)) ||
+            (self.set_params_for_card_id_drawdown!(charge_params) if self.has_valid_card_id?(charge_params))
       rescue Lightrail::LightrailArgumentError
       end
       raise Lightrail::LightrailArgumentError.new("Invalid charge_params: #{charge_params.inspect}")

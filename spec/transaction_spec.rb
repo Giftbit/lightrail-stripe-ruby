85e37af0d6933b6456b53c8ce4cc71ccc782237f
Tana Jukes <tana.j@lightrail.com>
2017-08-23 16:26:23 -0700

    Reformat Transaction spec

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,25 +3,31 @@ require "spec_helper"
 RSpec.describe Lightrail::Transaction do
   subject(:transaction) {Lightrail::Transaction}
 
-  describe ".create" do
-    it "posts a new drawdown transaction to a code" do
-      charge_params = {
-          value: -1,
-          currency: 'USD',
-          code: ENV['LIGHTRAIL_TEST_CODE'],
-      }
-      transac = transaction.create(charge_params, :code_drawdown)
-      expect(transac).to have_key('transactionId')
-    end
+  let(:code_charge_params) {{
+      value: -1,
+      currency: 'USD',
+      code: ENV['LIGHTRAIL_TEST_CODE'],
+  }}
 
-    it "posts a new drawdown transaction to a card ID" do
-      charge_params = {
-          value: -1,
-          currency: 'USD',
-          card_id: ENV['LIGHTRAIL_TEST_CARD_ID'],
-      }
-      transac = transaction.create(charge_params, :card_id_drawdown)
-      expect(transac).to have_key('transactionId')
+  let(:card_id_charge_params) {{
+      value: -1,
+      currency: 'USD',
+      card_id: ENV['LIGHTRAIL_TEST_CARD_ID'],
+  }}
+
+  describe ".create" do
+    context "when posting a drawdown transaction" do
+      it "charges a code with minimum parameters" do
+        transac = transaction.create(code_charge_params, :code_drawdown)
+        expect(transac).to have_key('transactionId')
+        expect(transac['transactionType']).to eq('DRAWDOWN')
+      end
+
+      it "charges a card with minimum parameters" do
+        transac = transaction.create(card_id_charge_params, :card_id_drawdown)
+        expect(transac).to have_key('transactionId')
+        expect(transac['transactionType']).to eq('DRAWDOWN')
+      end
     end
 
   end

f0c3584d35f0c0d62dd740c3cb9e3024e2953459
Tana Jukes <tana.j@lightrail.com>
2017-08-28 16:28:50 -0700

    Remove spec to check for presence of API key

--- spec/lightrail_stripe_spec.rb
+++ spec/lightrail_stripe_spec.rb
@@ -8,9 +8,4 @@ RSpec.describe Lightrail do
   it "stores the base URL for the Lightrail API" do
     expect(Lightrail.api_base).to eq('https://dev.lightrail.com/v1').or(eq('https://api.lightrail.com/v1'))
   end
-
-  it "stores a string for the API key" do
-    expect(Lightrail.api_key).to match(/[A-Z0-9]+/)
-  end
-
 end

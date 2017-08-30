a0a91df8e77d824992ce80694eda392113520da9
Tana Jukes <tana.j@lightrail.com>
2017-08-29 18:38:40 -0700

    Stub responses for Transaction spec

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,49 +3,66 @@ require "spec_helper"
 RSpec.describe Lightrail::Transaction do
   subject(:transaction) {Lightrail::Transaction}
 
+  let(:lightrail_connection) {Lightrail::Connection}
+
+  let(:example_code) {'this-is-a-code'}
+  let(:example_card_id) {'this-is-a-card-id'}
+  let(:example_transaction_id) {'this-is-a-transaction-id'}
+
   let(:code_charge_params) {{
       value: -1,
       currency: 'USD',
-      code: ENV['LIGHTRAIL_TEST_CODE'],
+      code: example_code,
   }}
 
   let(:card_id_charge_params) {{
       value: -1,
       currency: 'USD',
-      card_id: ENV['LIGHTRAIL_TEST_CARD_ID'],
+      card_id: example_card_id,
   }}
 
   let(:card_id_fund_params) {{
       value: 1,
       currency: 'USD',
-      card_id: ENV['LIGHTRAIL_TEST_CARD_ID'],
+      card_id: example_card_id,
   }}
 
   let(:card_id_pending_charge_params) {{
       value: -1,
       currency: 'USD',
-      card_id: ENV['LIGHTRAIL_TEST_CARD_ID'],
+      card_id: example_card_id,
       pending: true,
+  }}
+
+  let(:transaction_info) {{
+      cardId: example_card_id,
+      codeLastFour: 'TEST',
+      currency: 'USD',
+      transactionId: example_transaction_id,
+      userSuppliedId: '123-abc-456-def',
+      value: -1,
   }}
 
 
   describe ".charge_code" do
     context "when posting a drawdown transaction" do
       it "charges a code with minimum parameters" do
-        transac = transaction.charge_code(code_charge_params)
-        expect(transac).to have_key('transactionId')
-        expect(transac['transactionType']).to eq('DRAWDOWN')
+        expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/codes\/#{example_code}\/transactions/, hash_including(:value, :currency, :userSuppliedId)).and_return({"transaction" => {}})
+        transaction.charge_code(code_charge_params)
       end
 
-      it "charges a code first if both code and cardId are present"
+      it "charges a code first if both code and cardId are present" do
+        code_charge_params[:card_id] = example_card_id
+        expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/codes\/#{example_code}\/transactions/, hash_including(:value, :currency, :userSuppliedId)).and_return({"transaction" => {}})
+        transaction.charge_code(code_charge_params)
+      end
     end
 
     context "when posting a pending transaction" do
       it "posts a pending transaction to a code" do
         code_charge_params[:pending] = true
-        transac = transaction.charge_code(code_charge_params)
-        expect(transac).to have_key('transactionId')
-        expect(transac['transactionType']).to eq('PENDING_CREATE')
+        expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/codes\/#{example_code}\/transactions/, hash_including(:value, :currency, :userSuppliedId, pending: true)).and_return({"transaction" => {}})
+        transaction.charge_code(code_charge_params)
       end
     end
 
@@ -54,18 +71,16 @@ RSpec.describe Lightrail::Transaction do
   describe ".charge_card" do
     context "when posting a drawdown transaction" do
       it "charges a card with minimum parameters" do
-        transac = transaction.charge_card(card_id_charge_params)
-        expect(transac).to have_key('transactionId')
-        expect(transac['transactionType']).to eq('DRAWDOWN')
+        expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/cards\/#{example_card_id}\/transactions/, hash_including(:value, :currency, :userSuppliedId)).and_return({"transaction" => {}})
+        transaction.charge_card(card_id_charge_params)
       end
     end
 
     context "when posting a pending transaction" do
       it "posts a pending transaction to a card_id" do
         card_id_charge_params[:pending] = true
-        transac = transaction.charge_card(card_id_charge_params)
-        expect(transac).to have_key('transactionId')
-        expect(transac['transactionType']).to eq('PENDING_CREATE')
+        expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/cards\/#{example_card_id}\/transactions/, hash_including(:value, :currency, :userSuppliedId, pending: true)).and_return({"transaction" => {}})
+        transaction.charge_card(card_id_charge_params)
       end
     end
 
@@ -73,16 +88,15 @@ RSpec.describe Lightrail::Transaction do
 
   describe ".fund_card" do
     it "funds a card" do
-      transac = transaction.fund_card(card_id_fund_params)
-      expect(transac).to have_key('transactionId')
-      expect(transac['transactionType']).to eq('FUND')
+      expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/cards\/#{example_card_id}\/transactions/, hash_including(:value, :currency, :userSuppliedId)).and_return({"transaction" => {}})
+      transaction.fund_card(card_id_fund_params)
     end
 
     it "does not fund a code" do
       code_fund_params = {
           value: 1,
           currency: 'USD',
-          code: ENV['LIGHTRAIL_TEST_CODE'],
+          code: example_code,
       }
       expect {transaction.fund_card(code_fund_params)}.to raise_error(Lightrail::LightrailArgumentError)
     end
@@ -90,31 +104,22 @@ RSpec.describe Lightrail::Transaction do
 
   describe ".void" do
     it "cancels a pending transaction" do
-      pending = transaction.charge_card(card_id_pending_charge_params)
-      transac = transaction.void(pending)
-
-      expect(transac).to have_key('transactionId')
-      expect(transac['transactionType']).to eq('PENDING_VOID')
+      expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/cards\/#{example_card_id}\/transactions\/#{example_transaction_id}\/void/, Hash).and_return({"transaction" => {}})
+      transaction.void(transaction_info)
     end
   end
 
   describe ".capture" do
     it "captures a pending transaction" do
-      pending = transaction.charge_card(card_id_pending_charge_params)
-      transac = transaction.capture(pending)
-
-      expect(transac).to have_key('transactionId')
-      expect(transac['transactionType']).to eq('DRAWDOWN')
+      expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/cards\/#{example_card_id}\/transactions\/#{example_transaction_id}\/capture/, Hash).and_return({"transaction" => {}})
+      transaction.capture(transaction_info)
     end
   end
 
   describe ".refund" do
     it "refunds a drawdown transaction" do
-      original_transac = transaction.charge_card(card_id_charge_params)
-      transac = transaction.refund(original_transac)
-
-      expect(transac).to have_key('transactionId')
-      expect(transac['transactionType']).to eq('DRAWDOWN_REFUND')
+      expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/cards\/#{example_card_id}\/transactions\/#{example_transaction_id}\/refund/, Hash).and_return({"transaction" => {}})
+      transaction.refund(transaction_info)
     end
   end
 end

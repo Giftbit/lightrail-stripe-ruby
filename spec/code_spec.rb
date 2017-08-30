9df1200b6a61f58da85b1c5dcafbc4fb6b7150cf
Tana Jukes <tana.j@lightrail.com>
2017-08-29 18:14:26 -0700

    Stub responses for Code spec

--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -3,28 +3,41 @@ require "spec_helper"
 RSpec.describe Lightrail::Code do
   subject(:code) {Lightrail::Code}
 
+  let(:lightrail_connection) {Lightrail::Connection}
+
+  let(:example_code) {'this-is-a-code'}
+
   let(:charge_params) {{
-      value: -1,
+      value: 1,
       currency: 'USD',
-      code: ENV['LIGHTRAIL_TEST_CODE'],
+      code: example_code,
+  }}
+
+  let(:balance_response) {{
+      "principal" => {"currentValue" => 400, "state" => "ACTIVE", "programId" => "program-123456", "valueStoreId" => "value-123456"},
+      "attached" => [{"currentValue" => 50, "state" => "ACTIVE", "programId" => "program-789", "valueStoreId" => "value-2468"},
+                     {"currentValue" => 30, "state" => "EXPIRED", "programId" => "program-235", "valueStoreId" => "value-7643"}]
   }}
 
   describe ".charge" do
     it "posts a charge to a code" do
-      charge_response = code.charge(charge_params)
-      expect(charge_response).to have_key('transactionId')
+      expect(lightrail_connection).to receive(:make_post_request_and_parse_response).with(/codes\/#{example_code}\/transactions/, hash_including(:value, :currency, :userSuppliedId)).and_return({"transaction" => {}})
+      code.charge(charge_params)
     end
   end
 
   describe ".get_balance_details" do
     it "gets the balance details by cardId" do
-      expect(code.get_balance_details(ENV['LIGHTRAIL_TEST_CODE'])).to have_key('principal')
+      expect(lightrail_connection).to receive(:make_get_request_and_parse_response).with(/codes\/#{example_code}\/balance/).and_return({"balance" => {}})
+      code.get_balance_details(example_code)
     end
   end
 
   describe ".get_total_balance" do
     it "gets the total balance for a code" do
-      expect(code.get_total_balance(ENV['LIGHTRAIL_TEST_CODE'])).to be_a(Integer)
+      expect(code).to receive(:get_balance_details).with(example_code).and_return(balance_response)
+      balance = code.get_total_balance(example_code)
+      expect(balance).to be 450
     end
   end
 

bdcc27970ca097fdb855bcea156248c81b03f7d5
Tana Jukes <tana.j@lightrail.com>
2018-01-19 15:22:54 -0800

    More descriptive errors from split tender params validator
    Throw a descriptive error from any step of the validation if it fails.

--- lib/lightrail_stripe/split_tender_validator.rb
+++ lib/lightrail_stripe/split_tender_validator.rb
@@ -2,16 +2,18 @@ module Lightrail
   class SplitTenderValidator < Lightrail::Validator
 
     def self.validate_split_tender_charge_params! (split_tender_charge_params, lr_share=0)
-      begin
-        return true if ((split_tender_charge_params.is_a? Hash) &&
-            Lightrail::Validator.validate_amount!(split_tender_charge_params[:amount]) &&
-            (split_tender_charge_params[:amount] >= lr_share) &&
-            Lightrail::Validator.validate_currency!(split_tender_charge_params[:currency]) &&
-            (self.has_lightrail_payment_option?(split_tender_charge_params) ||
-                self.has_stripe_payment_option?(split_tender_charge_params)))
-      rescue Lightrail::LightrailArgumentError
-      end
-      raise Lightrail::LightrailArgumentError.new("Invalid split_tender_charge_params: #{split_tender_charge_params.inspect}")
+
+      raise Lightrail::LightrailArgumentError.new("Invalid split_tender_charge_params - must be a hash: #{split_tender_charge_params.inspect}") unless (split_tender_charge_params.is_a? Hash)
+
+      raise Lightrail::LightrailArgumentError.new("Invalid amount in split_tender_charge_params: #{split_tender_charge_params.inspect}") unless Lightrail::Validator.validate_amount!(split_tender_charge_params[:amount])
+
+      raise Lightrail::LightrailArgumentError.new("Amount in split_tender_charge_params less than specified Lightrail share of #{lr_share}: #{split_tender_charge_params.inspect}") unless (split_tender_charge_params[:amount] >= lr_share)
+
+      raise Lightrail::LightrailArgumentError.new("Invalid currency in split_tender_charge_params: #{split_tender_charge_params.inspect}") unless Lightrail::Validator.validate_currency!(split_tender_charge_params[:currency])
+
+      raise Lightrail::LightrailArgumentError.new("Must provide a payment method for either Lightrail or Stripe: #{split_tender_charge_params.inspect}") unless (self.has_lightrail_payment_option?(split_tender_charge_params) || self.has_stripe_payment_option?(split_tender_charge_params))
+
+      return true
     end
 
     def self.has_stripe_payment_option?(charge_params)
